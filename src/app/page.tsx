3addf08ec60cf194591689b6caddd321736906b2
Senan <senan996@gmail.com>
2024-10-21 19:46:40 +0200

    mobile aahahaskdjhfa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
       <section className="absolute top-0 left-0 w-full h-full m-0 p-0 bg-hero bg-no-repeat bg-cover bg-center z-0 grid items-center md:pt-[100px]">
         <div className="flex flex-col lg:flex-row mx-10 justify-around lg:items-center">
           <div className="flex flex-col gap-5 mb-10 lg:w-[50%] text-center lg:text-left">
-            <h1 className="text-white text-4xl md:text-6xl">Your Luxury Journey Begins Here</h1>
+            <h1 className="text-white hidden sm:block text-4xl md:text-6xl">
+              Your Luxury Journey Begins Here
+            </h1>
             <p className="text-white hidden sm:block">
               Discover the finest way to travel along the sun-kissed coastlines
               with South Coast Transfer
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface Props {
   type: string;
   placeholder?: string;
@@ -6,13 +8,29 @@ interface Props {
 }
 
 function Input({ type, placeholder, min, max }: Props) {
+  const [inputType, setInputType] = useState("text");
+
+  const handleFocus = () => {
+    if (type === "date" || type === "time") {
+      setInputType(type); // Cambia el tipo al correspondiente (date o time)
+    }
+  };
+
+  const handleBlur = () => {
+    if (type === "date" || type === "time") {
+      setInputType("text"); // Vuelve al tipo text cuando se pierde el foco
+    }
+  };
+
   return (
     <input
-      type={type}
+      type={inputType}
       placeholder={placeholder}
       className="w-full p-3 border border-blue-500 bg-black-400 text-white/80 placeholder-white/80 rounded outline-none"
       min={min}
       max={max}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     />
   );
 }
