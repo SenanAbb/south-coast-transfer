a1023b277caa7125e29fe5b10ab8486d4723b841
Senan <senan996@gmail.com>
2024-12-04 20:05:42 +0100

    Trying to fix input date on mobile

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Button from "./Button";
-import Input from "./Input";
-import DateInput from "./DateInput";
+import { useState } from 'react';
+import Button from './Button';
+import Input from './Input';
+import DateInput from './DateInput';
 
 function BookingForm() {
-  const [origin, setOrigin] = useState<string>("");
-  const [destination, setDestination] = useState<string>("");
+  const [origin, setOrigin] = useState<string>('');
+  const [destination, setDestination] = useState<string>('');
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [passengers, setPassengers] = useState<number | undefined>(undefined);
 
@@ -18,8 +18,8 @@ function BookingForm() {
 
   const check = () => {
     return (
-      origin === "" ||
-      destination === "" ||
+      origin === '' ||
+      destination === '' ||
       date === undefined ||
       passengers === undefined
     );
@@ -47,18 +47,19 @@ function BookingForm() {
           setDestination(e.currentTarget.value);
         }}
       />
-      <DateInput date={date} setDate={setDate} />
+      {/* <DateInput date={date} setDate={setDate} /> */}
+      <Input type="date" value="" placeholder="Date" onChange={() => {}} />
       <Input
         type="select"
         options={[
-          { value: "1", label: "1" },
-          { value: "2", label: "2" },
-          { value: "3", label: "3" },
-          { value: "4", label: "4" },
-          { value: "5", label: "5" },
-          { value: "6", label: "6" },
-          { value: "7", label: "7" },
-          { value: "8", label: "8" },
+          { value: '1', label: '1' },
+          { value: '2', label: '2' },
+          { value: '3', label: '3' },
+          { value: '4', label: '4' },
+          { value: '5', label: '5' },
+          { value: '6', label: '6' },
+          { value: '7', label: '7' },
+          { value: '8', label: '8' },
         ]}
         placeholder="Passengers"
         value={passengers}
